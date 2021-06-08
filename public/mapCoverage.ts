25c6caf0354f02d2a3547b8a8fcefad89baa1170
Lior Bragilevsky <lbragile.masc@gmail.com>
2021-06-07 21:48:00 -0700

    made both unit and integration tests work in parallel in GitHub workflow. Coverage merging update to read files properly depending on workflow vs. local

--- public/mapCoverage.ts
+++ public/mapCoverage.ts
@@ -8,7 +8,7 @@ const reporter = createReporter();
 const testTypes = ["unit", "integration"];
 
 testTypes.forEach((testType) => {
-  const coverage = require(`../coverage/coverage-${testType}-final.json`);
+  const coverage = require((process.env.GITHUB_WORKFLOW ? "./" : "../") + `coverage/coverage-${testType}-final.json`);
   Object.keys(coverage).forEach((filename) => map.addFileCoverage(coverage[filename]));
 });
 
