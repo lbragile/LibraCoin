2dcba85e09be333813af8da99c816f88fa3c07ef
Lior Bragilevsky <lbragile.masc@gmail.com>
2021-06-08 19:54:38 -0700

    removed test result combination since Codecov handles this functionality by default

--- public/file_rename.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fs = require("fs");
-
-const args = process.argv;
-fs.rename(args[args.length - 2], args[args.length - 1], (err) => {
-  if (err) console.error(err);
-});
